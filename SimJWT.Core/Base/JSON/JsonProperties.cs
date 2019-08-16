73a226091dd383ada71c07bb0a122899e99f0c4f
Richard <liujh88888@sina.cn>
2019-08-17 00:00:43 +0800

    add JsonProperties & JsonWriter

---
+++ SimJWT.Core/Base/JSON/JsonProperties.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimJWT.Core.Base.JSON
+{
+    public class JsonProperties
+    {
+        private Dictionary<string, string> _stringProperties;
+        private Dictionary<string, JsonProperties> _objectProperties;
+
+        public int StringPropertiesCount { get { return _stringProperties.Count; } }
+        public int ObjectPropertiesCount { get { return _objectProperties.Count; } }
+
+        public void AddStringProperty(string key, string value)
+        {
+            if (value == null || value == "") return;
+            _stringProperties.Add(key, value);
+        }
+
+        public void AddObjectProperty(string key, JsonProperties jp)
+        {
+            if (jp == null || jp.StringPropertiesCount == 0 || jp.ObjectPropertiesCount == 0) return;
+            _objectProperties.Add(key, jp);
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetStringPropertiesEnumerator()
+        {
+            return _stringProperties.GetEnumerator();
+        }
+
+        public IEnumerator<KeyValuePair<string,JsonProperties>> GetObjectPropertiesEnumerator()
+        {
+            return _objectProperties.GetEnumerator();
+        }
+    }
+}
---
+++ SimJWT.Core/Base/JSON/JsonWriter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimJWT.Core.Base.JSON
+{
+    public class JsonWriter
+    {
+        private StringBuilder _stringBuilder;
+        private JsonProperties _jsonProperties;
+
+        public JsonWriter(StringBuilder stringBuilder, JsonProperties jsonProperties)
+        {
+            _stringBuilder = stringBuilder;
+            _jsonProperties = jsonProperties;
+        }
+
+        private void StartWriteObject()
+        {
+            _stringBuilder.Append('{');
+        }
+
+        private void EndWriteObject()
+        {
+            _stringBuilder.Remove(_stringBuilder.Length - 2, 1);
+            _stringBuilder.Append('}');
+        }
+
+        private void AddOneProperty(string name, string value)
+        {
+            if (name == null || name == "" || value == null || value == "") return;
+            _stringBuilder.AppendFormat("\"{0}\":\"{1}\",", name, value);
+        }
+
+        private void AddOneProperty(string name, JsonProperties jsonProperties)
+        {
+            if (name == null || name == "") return;
+            var enumerator = jsonProperties.GetStringPropertiesEnumerator();
+            if (enumerator.MoveNext() == false) return;
+
+            _stringBuilder.AppendFormat("\"{0}\":", name);
+            StartWriteObject();
+
+            do
+            {
+                var current = enumerator.Current;
+                AddOneProperty(current.Key, current.Value);
+            }while (enumerator.MoveNext());
+
+            EndWriteObject();
+            _stringBuilder.Append(',');
+        }
+
+        public string WriteJsonString()
+        {
+            StartWriteObject();
+            var stringEnumerator = _jsonProperties.GetStringPropertiesEnumerator();
+            var objcetEnumerator = _jsonProperties.GetObjectPropertiesEnumerator();
+
+            if (stringEnumerator.MoveNext() == false) return "";
+
+            do
+            {
+                AddOneProperty(stringEnumerator.Current.Key, stringEnumerator.Current.Value);
+            } while (stringEnumerator.MoveNext());
+
+            if (objcetEnumerator.MoveNext() == false) return _stringBuilder.ToString();
+
+            do
+            {
+                AddOneProperty(objcetEnumerator.Current.Key, objcetEnumerator.Current.Value);
+            } while (objcetEnumerator.MoveNext());
+
+            EndWriteObject();
+
+            return _stringBuilder.ToString();
+        }
+    }
+}
